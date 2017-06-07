1024b3997f27066e1cb604df4d62bb0e0a243865
Corentin Remaud <corentin@billjobs.fr>
2017-06-07 16:15:25 +0200

    Update packaging

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ from setuptools import setup
 
 setup(
 	name='python-allrecipes',
-	version='0.1.2',
-	scripts=['allrecipes.py']
+	version='0.1.4',
+	description='Easy-to-use Python API for the allrecipes.com cokking website.',
+	packages=['allrecipes'],
+	url='https://github.com/remaudcorentin-dev/python-allrecipes',
+	author='Corentin Remaud',
+	author_email='remaudcorentin.dev@gmail.com',
+	license='MIT',
+	zip_safe=False,
+	install_requires=['bs4'],
 	)
